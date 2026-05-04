3b6fa98630153f0ad8961ebf2c45ec9cf7ae3ff3
git lysing098 <lyzing1021@gmail.com>
2026-05-04 11:07:26 +0700

    fix hambuger menu

--- resources/views/Frontend/components/Header.blade.php
+++ resources/views/Frontend/components/Header.blade.php
@@ -1,27 +1,28 @@
-<div x-data="{ open: false }" class="relative z-10 text-white capitalize">
+<div x-data="{ open: false }" class="text-white capitalize relative">
 
     <!-- TOP NAV -->
-    <div class="bg-[#0B0B54] font-semibold">
+    <div class="bg-[#0B0B54] font-semibold relative z-50">
         <div class="container mx-auto flex items-center justify-between py-5 px-5">
 
-            <!-- MOBILE + TABLET + SMALL LAPTOP BRAND -->
+            <!-- MOBILE BRAND -->
             <div class="flex items-center xl:hidden text-[#ED1C24] font-bold text-xl">
                 TK & D
             </div>
 
-            <!-- DESKTOP MENU (only true laptop/desktop now) -->
+            <!-- DESKTOP MENU -->
             <ul class="hidden xl:flex items-center gap-5 ml-auto">
-                <a href="/" class="capitalize hover:underline hover:decoration-[#ED1C24] hover:underline-offset-4 hover:text-[#ED1C24]">home</a>
-                <a href="/about-us" class="capitalize hover:underline hover:decoration-[#ED1C24] hover:underline-offset-4 hover:text-[#ED1C24]">about us</a>
-                <a href="/our-trading-products" class="capitalize hover:underline hover:decoration-[#ED1C24] hover:underline-offset-4 hover:text-[#ED1C24]">our trading products</a>
-                <a href="/export" class="capitalize hover:underline hover:decoration-[#ED1C24] hover:underline-offset-4 hover:text-[#ED1C24]">export</a>
-                <a href="/blog" class="capitalize hover:underline hover:decoration-[#ED1C24] hover:underline-offset-4 hover:text-[#ED1C24]">blog</a>
-                <a href="/activities" class="capitalize hover:underline hover:decoration-[#ED1C24] hover:underline-offset-4 hover:text-[#ED1C24]">activities</a>
-                <a href="/contact-us" class="capitalize hover:underline hover:decoration-[#ED1C24] hover:underline-offset-4 hover:text-[#ED1C24]">contact us</a>
+                <a href="/" class="hover:text-[#ED1C24]">home</a>
+                <a href="/about-us" class="hover:text-[#ED1C24]">about us</a>
+                <a href="/our-trading-products" class="hover:text-[#ED1C24]">products</a>
+                <a href="/export" class="hover:text-[#ED1C24]">export</a>
+                <a href="/blog" class="hover:text-[#ED1C24]">blog</a>
+                <a href="/activities" class="hover:text-[#ED1C24]">activities</a>
+                <a href="/contact-us" class="hover:text-[#ED1C24]">contact</a>
             </ul>
 
-            <!-- HAMBURGER (mobile + tablet + small laptop) -->
-            <button @click="open = !open" class="xl:hidden text-white text-2xl">
+            <!-- HAMBURGER -->
+            <button @click="open = !open"
+                class="xl:hidden text-white text-2xl relative z-50">
                 <i :class="open ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'"></i>
             </button>
 
@@ -29,38 +30,39 @@
     </div>
 
     <!-- BACKDROP -->
-    <div
-        x-show="open"
-        x-transition.opacity
-        class="fixed inset-0 bg-black/50 xl:hidden"
-        @click="open=false">
+    <div x-show="open"
+         x-cloak
+         x-transition.opacity
+         class="fixed inset-0 bg-black/50 xl:hidden z-40"
+         @click="open = false">
     </div>
 
     <!-- SIDEBAR -->
-    <div
-        x-show="open"
-        x-transition:enter="transition transform duration-300"
-        x-transition:enter-start="-translate-x-full"
-        x-transition:enter-end="translate-x-0"
-        x-transition:leave="transition transform duration-300"
-        x-transition:leave-start="translate-x-0"
-        x-transition:leave-end="-translate-x-full"
-        class="fixed top-0 left-0 h-full w-72 bg-[#0B0B54] p-6 space-y-6 xl:hidden z-50">
+    <div x-show="open"
+         x-cloak
+         x-transition:enter="transition transform duration-300"
+         x-transition:enter-start="-translate-x-full"
+         x-transition:enter-end="translate-x-0"
+         x-transition:leave="transition transform duration-300"
+         x-transition:leave-start="translate-x-0"
+         x-transition:leave-end="-translate-x-full"
+         class="fixed top-0 left-0 h-full w-72 bg-[#0B0B54] p-6 space-y-6
+                xl:hidden z-[9999]">
 
         <h2 class="text-[#ED1C24] text-xl font-bold">TK & D</h2>
 
         <a href="/" @click="open=false" class="block hover:text-[#ED1C24]">home</a>
         <a href="/about-us" @click="open=false" class="block hover:text-[#ED1C24]">about us</a>
-        <a href="/our-trading-products" @click="open=false" class="block hover:text-[#ED1C24]">our trading products</a>
+        <a href="/our-trading-products" @click="open=false" class="block hover:text-[#ED1C24]">products</a>
         <a href="/export" @click="open=false" class="block hover:text-[#ED1C24]">export</a>
         <a href="/blog" @click="open=false" class="block hover:text-[#ED1C24]">blog</a>
         <a href="/activities" @click="open=false" class="block hover:text-[#ED1C24]">activities</a>
-        <a href="/contact-us" @click="open=false" class="block hover:text-[#ED1C24]">contact us</a>
+        <a href="/contact-us" @click="open=false" class="block hover:text-[#ED1C24]">contact</a>
 
     </div>
 
     <!-- SUB HEADER -->
-    <div class="absolute top-0 left-0 w-1/3 overflow-hidden hidden xl:block">
+    <div class="absolute top-0 left-0 w-1/3 overflow-hidden hidden xl:block z-51">
 
         <div class="absolute inset-0 bg-[#ED1C24]
             [clip-path:polygon(0_0,100%_0,90%_100%,0_100%)]">
